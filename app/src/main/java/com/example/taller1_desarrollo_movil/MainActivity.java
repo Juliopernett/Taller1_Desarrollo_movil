185d47fc1c223d88b475f5939e659513f978007e
JulioPernett <jjpernett2008@gmail.com>
2019-07-09 12:21:58 -0500

    Bosquejo inicial de interfaz de usuario

--- app/src/main/java/com/example/taller1_desarrollo_movil/MainActivity.java
+++ app/src/main/java/com/example/taller1_desarrollo_movil/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static com.example.taller1_desarrollo_movil.R.mipmap.ic_launcher;
+import static com.example.taller1_desarrollo_movil.R.mipmap.ic_launcher_round;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
